f6f9cf579f587f8c9645868b4c3a63ec7bbca247
Valery Karitsky <valery.karitsky@gmail.com>
2023-03-12 13:12:34 +0100

    feat: accordion functionality implemented (Sun, Mar 12, 2023 13:13:00 PM)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('test');
+let accordion = document.getElementsByClassName('accordion-item');
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('mouseenter', function(){
+    if (!this.classList.contains('active')) {
+      this.classList.add('temp');
+    }
+  });
+
+  accordion[i].addEventListener('mouseleave', function(){
+    if (this.classList.contains('temp')) {
+      this.classList.remove('temp');
+    }
+  });
+
+  accordion[i].addEventListener('click', function(){
+    for (y = 0; y < accordion.length; y++) {
+      if (accordion[y] != this) {accordion[y].classList.remove('active')};
+    }
+    this.classList.toggle('active');
+    this.classList.remove('temp');
+  });
+
+}
